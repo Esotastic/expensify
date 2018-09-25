4c0e69c09f398acc030410dea56253c77333cf0a
Esotastic <chrishjimenez@gmail.com>
2018-09-24 20:51:03 -0400

    Broke AppRouter into multiple components

---
+++ src/components/NotFoundPage.js
@@ -0,0 +1,11 @@
+import {Link} from "react-router-dom";
+import React from "react";
+
+
+const NotFoundPage =() => (
+  <div>
+    This is the 404. <Link to="/">Back to home!</Link>
+  </div>
+);
+
+export default NotFoundPage;
