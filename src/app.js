02dcfb34f6b69d9932749366aa501e7277c43524
Esotastic <chrishjimenez@gmail.com>
2018-09-24 20:05:23 -0400

    install React router

--- src/app.js
+++ src/app.js
@@ -1,6 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route, Switch} from "react-router-dom";
 import "normalize-css/normalize.css";
 import "./styles/styles.scss";
 
-ReactDOM.render(<p>This is boilerplate</p>, document.getElementById("app"))
+const ExpenseDashboardPage = () => (
+  <div>
+    This is from the dashboard component. 
+  </div>
+);
+
+const AddExpensePage = () => (
+  <div>
+    This is from the add expense component. 
+  </div>
+);
+
+//add edit page and help page 
+
+const EditExpensePage =() => (
+  <div>
+    This is the edit expense page.
+  </div>
+);
+
+const HelpPage =() => (
+  <div>
+    This is the help page.
+  </div>
+);
+
+const NotFoundPage =() => (
+  <div>
+    This is the 404.
+  </div>
+);
+
+const routes = (
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={ExpenseDashboardPage} exact={true} />
+      <Route path="/create" component={AddExpensePage}/>
+      <Route path="/edit" component={EditExpensePage}/>
+      <Route path="/help" component={HelpPage}/>
+      <Route  component={NotFoundPage}/>
+    </Switch>
+  </BrowserRouter>
+);
+
+ReactDOM.render(routes, document.getElementById("app"))
